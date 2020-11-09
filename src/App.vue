4895d2c27fd3804eb0b9fc18e15dac12130ec7bc
Josh Fullmer <josh.fullmer@keap.com>
2020-11-09 10:05:29 -0700

    additional scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,12 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <teleport to="head">
+    <title>{{ title }}</title>
+  </teleport>
+  {{ title }}
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts" setup>
+import { ref } from 'vue'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+export const title = ref('Babbler: A twitter clone')
+</script>
