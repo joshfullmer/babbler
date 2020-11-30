29db78d01e37d41cf61c5e0a57aab913c924bee8
Josh Fullmer <josh.fullmer@keap.com>
2020-11-29 20:31:06 -0700

    layout cleanup

--- src/App.vue
+++ src/App.vue
@@ -6,39 +6,48 @@
       <section class="bg-green-500 flex-1">
         <h1>Hi</h1>
       </section>
+
       <section class="bg-red-500 flex-1 flex flex-col items-center">
         <form class="flex">
-          <div class="flex flex-col">
+          <fieldset class="flex flex-col">
             <label for="user">Phone, Email, or Username</label>
             <input
               name="user"
               type="text"
             >
-          </div>
-          <div class="flex flex-col">
+          </fieldset>
+
+          <fieldset class="flex flex-col">
             <label for="password">Password</label>
             <input
               name="password"
               type="password"
             >
             <a href="#">Forgot password?</a>
-          </div>
+          </fieldset>
+
           <button type="submit">
             Log in
           </button>
         </form>
+
         <div class="flex flex-col">
           <img
             src=""
             alt=""
           >
+
           <h3><strong>See what’s happening in the world right now</strong></h3>
+
           <span>Join Twitter today.</span>
+
           <button>Sign up</button>
+
           <button>Log in</button>
         </div>
       </section>
     </div>
+
     <footer class="bg-pink-900 h-12">
       <h1>Text</h1>
     </footer>
@@ -50,15 +59,7 @@ import { defineComponent } from 'vue'
 
 import PageTitle from './components/PageTitle.vue'
 
-import { useAppState } from './state/app.state'
-
 export default defineComponent({
-  components: { PageTitle },
-
-  setup () {
-    const { pageTitle } = useAppState()
-
-    return { pageTitle }
-  }
+  components: { PageTitle }
 })
 </script>
